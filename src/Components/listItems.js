ba4f62f91bbd8f00c7be898a531233453d704d32
ayrtoncu <bc.ayrton@gmail.com>
2022-11-29 11:35:54 -0500

    #18 Component Chartjs create and config

---
+++ src/Components/listItems.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import ListSubheader from "@mui/material/ListSubheader";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+
+export const MainListItems = ({pages, seeScreen}) => {
+
+  return (
+    <div>
+      {
+        pages.map((screen, index) => {
+          return (
+            <ListItemButton key={ screen.title} onClick={()=> seeScreen({title:screen.title, select:index})}>
+              <ListItemIcon>{screen.icon}</ListItemIcon>
+              <ListItemText primary={screen.title} />
+            </ListItemButton>
+          );
+        })
+      }
+    </div>
+  );
+};
+
+export const secondaryListItems = (
+  <React.Fragment>
+    <ListSubheader component="div" inset>
+      Saved reports
+    </ListSubheader>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Current month" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Last quarter" />
+    </ListItemButton>
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Year-end sale" />
+    </ListItemButton>
+  </React.Fragment>
+);
