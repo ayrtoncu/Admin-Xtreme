0133e1ca37737b85319c88ba20047addef5ae3b5
ayrtoncu <bc.ayrton@gmail.com>
2022-11-29 11:42:02 -0500

    #19 Index.js limpiado

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import DashboardContent from './Components/Dashboard';
 import Root from './Pages/Root';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
