6024fa145c828aceffb034ca79d49ad81696fdb6
ayrtoncu <bc.ayrton@gmail.com>
2022-11-29 11:42:15 -0500

    #20 Root.js Limpiado

--- src/Pages/Root.js
+++ src/Pages/Root.js
@@ -1,28 +1,16 @@
 import React from "react";
 import {
   createBrowserRouter,
-  RouterProvider,
   Routes,
   Route,
   BrowserRouter,
 } from "react-router-dom";
 import Dashboard from "../Components/Dashboard";
-import DashboardScrean from "../Screens/DashboardScrean";
 import { AuthProvider } from "../services/AuthProvider";
 import PrivateRoute from "../services/PrivateRoute";
 import SignInSide from "./Login";
 
 const Root = (props) => {
-  const router = createBrowserRouter([
-    {
-      path: "/login",
-      element: <SignInSide />,
-    },
-    {
-      path: "/",
-      element: <Dashboard />,
-    },
-  ]);
   return (
     <div>
       <AuthProvider>
