b51960abb60b84ba2f50b9f05c6a9e19bc60be82
ayrtoncu <bc.ayrton@gmail.com>
2022-11-29 11:33:43 -0500

    #12 Private Route Create

---
+++ src/services/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React, { useContext } from "react";
+import { Route, Routes, Router, Outlet, Navigate } from "react-router-dom";
+import { AuthContext } from "./AuthProvider";
+import Dashboard from "../Components/Dashboard";
+
+const PrivateRoute = ({ component: RouteComponent, ...otherProps }) => {
+  const { currentUser } = useContext(AuthContext);
+  return !!currentUser ? <Dashboard/>: <Navigate to ="/login"/>
+};
+
+export default PrivateRoute;
